b60c8cc4009555ffc79b002b179b18be2a7ad08b
Jing Wang <yinyinxiaozi@gmail.com>
2021-11-20 22:30:51 +0800

    注册全局request变量

--- www/src/main.js
+++ www/src/main.js
@@ -4,10 +4,12 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import router from './router'
 import store from './stores'
+import request from '@/utils/request'
 import './permission'
 
 
 const app = createApp(App)
+app.config.globalProperties.$request = request
 app.use(ElementPlus)
 app.use(router)
 app.use(store)
