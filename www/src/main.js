0fbe3aabb143c19601a72004384d7ee39cd2cb26
Jing Wang <yinyinxiaozi@gmail.com>
2021-11-21 16:01:41 +0800

    Update main.js
    本地中文化

--- www/src/main.js
+++ www/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import 'element-plus/dist/index.css'
 import router from './router'
 import store from './stores'
@@ -10,7 +11,7 @@ import './permission'
 
 const app = createApp(App)
 app.config.globalProperties.$request = request
-app.use(ElementPlus)
+app.use(ElementPlus,{ locale: zhCn})
 app.use(router)
 app.use(store)
 app.mount('#app')
