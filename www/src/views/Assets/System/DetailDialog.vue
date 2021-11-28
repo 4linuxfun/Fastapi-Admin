9edf3dc766a9f50ad4a4c0a6d881fa49ca2cf8f4
Jing Wang <yinyinxiaozi@gmail.com>
2021-11-28 20:45:51 +0800

    Update DetailDialog.vue
    table形式展示数据

--- www/src/views/Assets/System/DetailDialog.vue
+++ www/src/views/Assets/System/DetailDialog.vue
@@ -1,57 +1,67 @@
 <template>
 	<!-- 详细信息、单数据更新页面 -->
-	<el-dialog :model-value="visible" :title="title" width="30%" @close="$emit('update:visible',false)" destroy-on-close>
-		<el-form :model="formData" :disabled="disabled" label-width="100px">
-			<el-form-item label="资产类型">
-				<el-input v-model="formData.category" disabled></el-input>
-			</el-form-item>
-			<el-form-item label="管理员">
-				<el-input v-model="formData.manager"></el-input>
-			</el-form-item>
-			<el-form-item label="区域">
-				<el-input v-model="formData.area"></el-input>
-			</el-form-item>
-			<el-form-item label="使用人">
-				<el-input v-model="formData.user"></el-input>
-			</el-form-item>
-			<template v-for="(value,name) in formData.info" :key="name">
-				<el-form-item :label="name">
-					<el-input v-model="formData.info[name]"></el-input>
-				</el-form-item>
+	<el-dialog :model-value="visible" :title="title" width="70%" @close="$emit('update:visible',false)"
+		destroy-on-close>
+		<table border="1" style="width: 100%;">
+			<tr>
+				<td>资产类型</td>
+				<td>{{formData.category}}</td>
+				<td>管理员</td>
+				<td>{{formData.manager}}</td>
+			</tr>
+			<tr>
+				<td>区域</td>
+				<td>{{formData.area}}</td>
+				<td>使用人</td>
+				<td>{{formData.user}}</td>
+			</tr>
+			<template v-for="(item,index) in arrayData" :key="item">
+				<tr v-if="index%2 == 0">
+					<td>{{arrayData[index].name}}</td>
+					<td>{{arrayData[index].value}}</td>
+					<td>{{arrayData[index+1].name}}</td>
+					<td>{{arrayData[index+1].value}}</td>
+				</tr>
 			</template>
-			<el-form-item v-if="disabled == false">
-				<el-button type="danger" @click="$emit('update:visible',false)">取消</el-button>
-				<el-button type="primary" @click="updateFields(formData)">更新</el-button>
-			</el-form-item>
-		</el-form>
-	
+		</table>
 	</el-dialog>
-	
+
 </template>
 
 <script>
 	import request from '@/utils/request'
 	export default {
-		props: ['data','visible',"title","disabled"],
+		props: ['data', 'visible', "title", "disabled"],
 		emits: ['update:data', 'reload', 'update:visible'],
 		// created() {
 		// 	console.log(this.data)
 		// },
+		mounted() {
+			for(let [key,value] of Object.entries(this.formData)){
+				if(key === 'info'){
+					for(let [infoKey,infoValue] of Object.entries(value)){
+						this.arrayData.push({name:infoKey,value:infoValue})
+					}
+				} 
+			}
+			console.log(this.arrayData)
+		},
 		data() {
 			return {
 				formData: this.data,
+				arrayData: [],
 			}
 		},
 		methods: {
-			updateFields(fields){
+			updateFields(fields) {
 				console.log(fields)
-				this.$emit('update:visible',false)
+				this.$emit('update:visible', false)
 				this.$emit('reload')
 				request({
-					url:"/api/assets/update_category_detail",
-					method:"post",
-					data:fields
-				}).then(()=>{
+					url: "/api/assets/update_category_detail",
+					method: "post",
+					data: fields
+				}).then(() => {
 					console.log('更新成功')
 				})
 			},
