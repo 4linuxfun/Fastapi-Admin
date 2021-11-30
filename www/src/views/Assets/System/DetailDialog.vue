3f2910803a23e72fb9ea6543c3693684ee3c15e6
Jing Wang <yinyinxiaozi@gmail.com>
2021-11-30 10:20:57 +0800

    二分显示 fix #37

--- www/src/views/Assets/System/DetailDialog.vue
+++ www/src/views/Assets/System/DetailDialog.vue
@@ -1,71 +1,22 @@
 <template>
 	<!-- 详细信息、单数据更新页面 -->
-	<el-dialog :model-value="visible" :title="title" width="70%" @close="$emit('update:visible',false)"
+	<el-dialog :model-value="visible" title="数据详情" width="70%" @close="$emit('update:visible',false)"
 		destroy-on-close>
-		<table border="1" style="width: 100%;">
-			<tr>
-				<td>资产类型</td>
-				<td>{{formData.category}}</td>
-				<td>管理员</td>
-				<td>{{formData.manager}}</td>
-			</tr>
-			<tr>
-				<td>区域</td>
-				<td>{{formData.area}}</td>
-				<td>使用人</td>
-				<td>{{formData.user}}</td>
-			</tr>
-			<template v-for="(item,index) in arrayData" :key="item">
-				<tr v-if="index%2 == 0">
-					<td>{{arrayData[index].name}}</td>
-					<td>{{arrayData[index].value}}</td>
-					<td>{{arrayData[index+1].name}}</td>
-					<td>{{arrayData[index+1].value}}</td>
-				</tr>
-			</template>
-		</table>
+		<el-descriptions :border="true">
+			<el-descriptions-item label="资产类型">{{data.category}}</el-descriptions-item>
+			<el-descriptions-item label="管理员">{{data.manager}}</el-descriptions-item>
+			<el-descriptions-item label="区域">{{data.area}}</el-descriptions-item>
+			<el-descriptions-item label="使用人">{{data.user}}</el-descriptions-item>
+			<el-descriptions-item v-for="(value,name) in data.info" :key="name"  :label="name">{{value}}</el-descriptions-item>
+		</el-descriptions>
 	</el-dialog>
 
 </template>
 
 <script>
-	import request from '@/utils/request'
 	export default {
-		props: ['data', 'visible', "title", "disabled"],
-		emits: ['update:data', 'reload', 'update:visible'],
-		// created() {
-		// 	console.log(this.data)
-		// },
-		mounted() {
-			for(let [key,value] of Object.entries(this.formData)){
-				if(key === 'info'){
-					for(let [infoKey,infoValue] of Object.entries(value)){
-						this.arrayData.push({name:infoKey,value:infoValue})
-					}
-				} 
-			}
-			console.log(this.arrayData)
-		},
-		data() {
-			return {
-				formData: this.data,
-				arrayData: [],
-			}
-		},
-		methods: {
-			updateFields(fields) {
-				console.log(fields)
-				this.$emit('update:visible', false)
-				this.$emit('reload')
-				request({
-					url: "/api/assets/update_category_detail",
-					method: "post",
-					data: fields
-				}).then(() => {
-					console.log('更新成功')
-				})
-			},
-		},
+		props: ['data', 'visible',],
+		emits:['update:visible']
 	}
 </script>
 
