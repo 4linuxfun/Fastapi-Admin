e94c747536e0a74eb81370a9d75b09f7a4bb18a5
4linuxfun <yinyinxiaozi@gmail.com>
2022-06-06 14:02:27 +0800

    增加调试模式启动脚本

--- myapp.py
+++ myapp.py
@@ -1,5 +1,5 @@
 import uvicorn
-from .server.main import app
+from server.main import app
 
 if __name__ == '__main__':
-    uvicorn.run(app,host="0.0.0.0",port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
