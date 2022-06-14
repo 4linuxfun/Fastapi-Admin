5644d554be703071018bcdf0fea21b12093bc93c
4linuxfun <yinyinxiaozi@gmail.com>
2022-06-14 09:30:51 +0800

    添加前端配置文件后，URL路径使用绝对路径

--- www/src/api/menus.js
+++ www/src/api/menus.js
@@ -1,9 +1,9 @@
 import { GET, POST, PUT, DELETE } from '@/utils/request'
 
 // 菜单接口
-export const GetAllMenus = (q) => GET('api/menus', { q })
-export const PostNewMenu = (menu) => POST('api/menus', menu )
-export const PutMenu = (menu) => PUT('api/menus', menu)
-export const DeleteMenu = (id) => DELETE('api/menus/' + id)
-export const GetMenuApis = (id) => GET('api/menus/' + id + '/apis')
-export const GetMenuTreeApis = () => GET('api/menus/tree_apis')
+export const GetAllMenus = (q) => GET('/api/menus', { q })
+export const PostNewMenu = (menu) => POST('/api/menus', menu )
+export const PutMenu = (menu) => PUT('/api/menus', menu)
+export const DeleteMenu = (id) => DELETE('/api/menus/' + id)
+export const GetMenuApis = (id) => GET('/api/menus/' + id + '/apis')
+export const GetMenuTreeApis = () => GET('/api/menus/tree_apis')
