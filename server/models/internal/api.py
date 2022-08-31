035ec93a31250cc90c53af40b642222c28017eab
4linuxfun <yinyinxiaozi@gmail.com>
2022-08-31 09:41:51 +0800

    更新model定义

--- server/models/internal/api.py
+++ server/models/internal/api.py
@@ -1,5 +1,5 @@
 from typing import List, TYPE_CHECKING
-from sqlmodel import SQLModel, Field, Relationship
+from sqlmodel import SQLModel, Field, Relationship, Column, Integer
 from .relationships import MenuApis
 
 if TYPE_CHECKING:
@@ -7,16 +7,16 @@ if TYPE_CHECKING:
 
 
 class ApiBase(SQLModel):
-    tags: str
-    path: str
-    method: str
-    summary: str
-    deprecated: int = Field(default=0)
+    tags: str = Field(max_length=10, sa_column_kwargs={'comment': '标签'})
+    path: str = Field(max_length=50, sa_column_kwargs={'comment': 'API路径'})
+    method: str = Field(max_length=10, sa_column_kwargs={'comment': 'HTTP方法'})
+    summary: str = Field(max_length=20, sa_column_kwargs={'comment': '描述'})
+    deprecated: bool = Field(default=False, sa_column_kwargs={'comment': '是否废弃'})
 
 
 class Api(ApiBase, table=True):
     __tablename__ = 'sys_api'
-    id: int = Field(default=None, primary_key=True)
+    id: int = Field(sa_column=Column('id', Integer, primary_key=True, autoincrement=True))
     menus: List['Menu'] = Relationship(back_populates='apis', link_model=MenuApis)
 
 
