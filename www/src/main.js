0ddbccebc46a100cbcdc55837bc447d0b3a34e5b
4linuxfun <yinyinxiaozi@gmail.com>
2022-11-03 08:54:58 +0800

    前端导入所有的icon组件，方便后期动态修改icon fix #79

--- www/src/main.js
+++ www/src/main.js
@@ -1,6 +1,7 @@
 import {createApp} from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import 'element-plus/dist/index.css'
 import router from './router'
@@ -11,10 +12,15 @@ import './permission'
 
 
 const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
 app.config.globalProperties.$request = request
 app.use(ElementPlus, {locale: zhCn})
 app.use(router)
 app.use(createPinia())
+
 app.directive('permission', {
   mounted(el, binding) {
     console.log('permission run')
