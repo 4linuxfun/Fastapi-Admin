97a2f655a289b7caf8ba0962772750e7e3fafdf9
4linuxfun <yinyinxiaozi@gmail.com>
2022-11-03 16:01:45 +0800

    删除前端不必要的页面文件

--- www/src/api/categories.js
+++
@@ -1,8 +0,0 @@
-import {GET,POST,PUT} from '@/utils/request'
-
-// 资产类别接口
-export const GetCategories = (search) =>GET('api/categories',{search})
-export const GetCategoryDetail = (id) =>GET('api/categories/'+id +'/detail')
-export const GetCategoryFields = (id) =>GET('api/categories/'+id+'/fields')
-export const PostCategory = (category) => POST('api/categories',category)
-export const PutCategory = (id,category) => PUT('api/categories/'+id,category)
--- www/src/api/sysapi.js
+++
@@ -1,9 +0,0 @@
-import {GET, POST, PUT, DELETE} from '@/utils/request'
-
-export const GetSysApis = (q, direction, id, limit, offset_page) => GET('/api/sysapis', {
-  q,
-  direction,
-  id,
-  limit,
-  offset_page
-})
