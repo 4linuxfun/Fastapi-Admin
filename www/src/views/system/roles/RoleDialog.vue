04edd6d0749de9bea8b85a1dd6a187829e1890ad
4linuxfun <yinyinxiaozi@gmail.com>
2022-11-04 13:44:26 +0800

    优化按钮权限横排显示

--- www/src/views/system/roles/RoleDialog.vue
+++ www/src/views/system/roles/RoleDialog.vue
@@ -16,7 +16,7 @@
       </el-form-item>
       <el-form-item label="菜单权限">
         <el-tree ref="menuTree" :data="menus" :props="defaultProps" accordion show-checkbox node-key="id"
-                 :default-checked-keys="enables" check-strictly></el-tree>
+                 :default-checked-keys="enables" check-strictly/>
       </el-form-item>
       <el-form-item>
         <el-button @click="$emit('update:visible', false)">取消</el-button>
@@ -42,7 +42,8 @@
   const menus = ref([])
   const defaultProps = reactive({
     children: 'children',
-    label: 'name'
+    label: 'name',
+    class: customNodeClass
   })
   const enables = ref([])
   const category = ref([])
@@ -99,8 +100,19 @@
     emit('update:visible', false)
   }
 
+  function customNodeClass(data,node){
+    if (data.type === "subPage"){
+      return 'is-btn'
+    }
+    return null
+  }
+
   GetInfo()
 </script>
 
 <style>
+.el-tree-node.is-expanded.is-btn > .el-tree-node__children{
+  display: flex;
+  flex-direction: row;
+}
 </style>
