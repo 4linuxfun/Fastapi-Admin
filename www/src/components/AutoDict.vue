4f41bc225dea899ea62bc9bacfda844f0e29f1cc
4linuxfun <yinyinxiaozi@gmail.com>
2022-11-14 14:14:47 +0800

    更新AutoDict组件部分问题

--- www/src/components/AutoDict.vue
+++ www/src/components/AutoDict.vue
@@ -1,7 +1,7 @@
 <template>
   <!--数据字典自动带出组件。通过提供的URL获取信息，并根据需要选择相应的组件进行展示-->
   <template v-if="type==='select'">
-    <el-select  v-model="value" @change="handleUpdate">
+    <el-select v-model="value" @change="handleUpdate">
       <el-option v-for="item in itemArray" :key="item.value" :label="item.label" :value="item.value"/>
     </el-select>
   </template>
@@ -18,7 +18,7 @@
 
   const props = defineProps(['type', 'value', 'code'])
   const emit = defineEmits(['update:value'])
-  const {element, value, code} = toRefs(props)
+  const {type, value, code} = toRefs(props)
   const itemArray = ref(null)
 
   function handleUpdate(currentValue) {
