e206c660c97b418f7161a82f4167546a679f7e81
4linuxfun <yinyinxiaozi@gmail.com>
2022-11-17 16:05:59 +0800

    前端：创建重置按钮组件ResetButton

---
+++ www/src/components/ResetButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-button type="primary" :icon="Refresh" @click="handleReset">重置</el-button>
+</template>
+
+<script setup>
+  import {Refresh} from '@element-plus/icons-vue'
+  import {toRefs} from 'vue'
+
+  const props = defineProps(['searchRef'])
+  const emit = defineEmits(['handleSearch'])
+  const {searchRef} = toRefs(props)
+
+  function handleReset() {
+    searchRef.value.resetFields()
+    emit('handleSearch')
+  }
+</script>
+
+<style scoped>
+
+</style>
