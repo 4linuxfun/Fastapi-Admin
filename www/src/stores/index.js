2127b71f82139e1f59ca65b1969e2fa9115ce73d
4linuxfun <yinyinxiaozi@gmail.com>
2023-01-10 08:48:35 +0800

    前端：使用setup语法重写user store

--- www/src/stores/index.js
+++ www/src/stores/index.js
@@ -1,4 +1,5 @@
 import {defineStore} from 'pinia'
+import {ref} from 'vue'
 import {
   requestLogin,
   GetUserInfo,
@@ -9,72 +10,71 @@ import {
   removeToken
 } from '@/utils/auth'
 
-export const useStore = defineStore('user', {
-    state: () => {
-      return {
-        uid: '',
-        token: '',
-        name: '',
-        avatar: '',
-        asyncRoutes: [],
-        buttons:[],
-      }
+export const useStore = defineStore('user', () => {
 
-    },
+    const uid = ref('')
+    const token = ref('')
+    const name = ref(null)
+    const avatar = ref(null)
+    const asyncRoutes = ref([])
+    const buttons = ref([])
 
-    actions: {
-      //执行登录请求，获取token
-      logIn(userInfo) {
-        const username = userInfo.username
-        const password = userInfo.password
-        const rememberMe = userInfo.rememberMe
-        console.log('user login actions')
-        return new Promise((resolve, reject) => {
-          requestLogin(username, password).then((response) => {
-            console.log(response)
-            setToken(response.token, rememberMe)
-            this.token = response.token
-            this.uid = response.uid
-            resolve()
-          }).catch((error) => {
-            reject(error)
-          })
-        })
-      },
-      // 获取用户状态信息
-      getInfo() {
-        return new Promise((resolve, reject) => {
-          console.log('get user info')
-          GetUserInfo(this.uid).then(response => {
-            this.name = response.name
-            this.avatar = response.avatar
-            resolve(response)
-          }).catch(error => {
-            reject(error)
-          })
-        })
-      },
-      logOut() {
-        return new Promise((resolve) => {
-          this.$reset()
-          removeToken()
+    //执行登录请求，获取token
+    function logIn(userInfo) {
+      const username = userInfo.username
+      const password = userInfo.password
+      const rememberMe = userInfo.rememberMe
+      console.log('user login actions')
+      return new Promise((resolve, reject) => {
+        requestLogin(username, password).then((response) => {
+          console.log(response)
+          setToken(response.token, rememberMe)
+          token.value = response.token
+          uid.value = response.uid
           resolve()
+        }).catch((error) => {
+          reject(error)
         })
-      },
-      // 获取用户权限列表
-      getPermission() {
-        return new Promise((resolve, reject) => {
-          GetUserPermission().then(response => {
-            console.log('permission response is:')
-            console.log(response)
-            this.asyncRoutes = response.menus
-            this.buttons = response.btns
-            resolve(response)
-          }).catch(error => {
-            reject(error)
-          })
-        })
-      }
+      })
     }
+
+    // 获取用户状态信息
+    function getInfo() {
+      return new Promise((resolve, reject) => {
+        console.log('get user info')
+        GetUserInfo(this.uid).then(response => {
+          name.value = response.name
+          avatar.value = response.avatar
+          resolve(response)
+        }).catch(error => {
+          reject(error)
+        })
+      })
+    }
+
+    function logOut() {
+      return new Promise((resolve) => {
+        // this.$reset()
+        removeToken()
+        resolve()
+      })
+    }
+
+    // 获取用户权限列表
+    function getPermission() {
+      return new Promise((resolve, reject) => {
+        GetUserPermission().then(response => {
+          console.log('permission response is:')
+          console.log(response)
+          asyncRoutes.value = (response.menus)
+          buttons.value = (response.btns)
+          resolve(response)
+        }).catch(error => {
+          reject(error)
+        })
+      })
+    }
+
+    return {uid, token, name, avatar, asyncRoutes, buttons, getInfo, logIn, logOut, getPermission}
   }
 )
