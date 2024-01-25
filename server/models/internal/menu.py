1138ddf4bc62ba834f30e4fc7a5e0bd8e4140af5
4linuxfun <yinyinxiaozi@gmail.com>
2024-01-25 09:49:02 +0800

    后端：修复新增菜单icon、sort字段可选

--- server/models/internal/menu.py
+++ server/models/internal/menu.py
@@ -6,13 +6,13 @@ from .relationships import RoleMenu
 class MenuBase(SQLModel):
     id: Optional[int] = Field(sa_column=Column('id', Integer, primary_key=True, autoincrement=True))
     name: str = Field(max_length=20, sa_column_kwargs={'comment': '菜单名'})
-    icon: Optional[str] = Field(max_length=50, sa_column_kwargs={'comment': 'Icon图标'})
+    icon: Optional[str] = Field(default=None, max_length=50, sa_column_kwargs={'comment': 'Icon图标'})
     path: Optional[str] = Field(max_length=100, sa_column_kwargs={'comment': '路径'})
     component: Optional[str] = Field(max_length=50, sa_column_kwargs={'comment': '组件'})
     auth: Optional[str] = Field(max_length=50, sa_column_kwargs={'comment': '授权标识'})
     type: str = Field(max_length=10, sa_column_kwargs={'comment': '类型'})
     parent_id: Optional[int] = Field(default=None, sa_column_kwargs={'comment': '父级ID'})
-    sort: Optional[float] = Field(sa_column_kwargs={'comment': '菜单排序'})
+    sort: Optional[float] = Field(default=None, sa_column_kwargs={'comment': '菜单排序'})
     enable: bool = Field(default=True, sa_column=Column(Boolean, comment='启用'))
 
 
