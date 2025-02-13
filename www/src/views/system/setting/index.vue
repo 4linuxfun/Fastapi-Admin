9cb6b5986623a873e45c93bfba0ed1a93ce7d8f9
4linuxfun <yinyinxiaozi@gmail.com>
2025-02-13 14:54:20 +0800

    update：其他

---
+++ www/src/views/system/setting/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-tabs tab-position="left" style="margin-top: 10px">
+    <el-tab-pane label="安全设置">安全设置111222</el-tab-pane>
+    <el-tab-pane label="密钥设置">密钥设置222</el-tab-pane>
+    <el-tab-pane label="推送服务设置">推送服务设置</el-tab-pane>
+    <el-tab-pane label="报警服务设置">报警服务设置</el-tab-pane>
+    <el-tab-pane label="开放服务设置">开放服务设置</el-tab-pane>
+  </el-tabs>
+</template>
+
+<script>
+  export default {
+    name: '系统设置'
+  }
+</script>
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
